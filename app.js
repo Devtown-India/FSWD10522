b6abd3a2d7fbab2354d8ceca57d855edda56a2b0
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-06 21:23:50 +0530

    class

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
-// class Car{
-//     constructor(model,make){
-//         this.make = make;
-//         this.model = model;
-//     }
+class Student {
+  constructor(name, batch) {
+    this.name = name;
+    this.batch = batch;
+  }
 
-//     move(){
-//         console.log("car is moving")
-//     }
-// }
+  study() {
+    console.log("studying");
+  }
+}
 
-// const car1 = new Car('BMW','X5');
+const s1 = new Student("John", "FSWD-10522");
+const s2 = new Student("Appleeed", "FSWD-10522");
 
-const arr = [1,2,3,4,5,6,7,8,9,10];
-
-// const sum = arr.reduce((a,b)=>a+b)
-const sum = arr.reduce(function(a,b){
-    // console.log(a,b)
-    return a*b
-})
-
-console.log(sum)
-
-// const newArr = arr.filter((item)=> item % 2 === 0)
-// console.log(newArr)
-
-// const ele = arr.find(function(item){
-//     return item==10
-// })
-
-// const newArr = []
-
-// arr.forEach(function(e){
-//     if(e%2 === 0){
-//         newArr.push(e);
-//     }
-// })
+console.log(s1)
+console.log(s2)
