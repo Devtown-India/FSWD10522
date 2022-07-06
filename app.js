498a8edfd8e70df94df09605844b782e8c15eca8
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-06 20:37:45 +0530

    factory function

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-class Car{
-    constructor(model,make){
-        this.make = make;
-        this.model = model;
-    }
+// class Car{
+//     constructor(model,make){
+//         this.make = make;
+//         this.model = model;
+//     }
 
-    move(){
-        console.log("car is moving")
-    }
+//     move(){
+//         console.log("car is moving")
+//     }
+// }
+
+// const car1 = new Car('BMW','X5');
+
+const Car = function(make,model,year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    return this
 }
 
-const car1 = new Car('BMW','X5');
-const car2 = new Car(19);
-
-// console.log(car1.model)
+const car1 = new Car("Mercedes Benz","GTR pro",2022);
+console.log(car1)
