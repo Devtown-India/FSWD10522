3b8a427274e1d77973bd6733a76da2e18147f269
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-06 20:15:08 +0530

    change

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("something 1234")
+const obj = {
+    name: 'John',
+    car:"SLS"
+}
+console.log(obj)
+
+class Car{
+    constructor(model,make){
+        this.make = make;
+        this.model = model;
+    }
+}
+
+
+// console.log(window)
+
+// const car = {
+//     make:"Mercedes",
+//     model:"GLE-63",
+//     move:function(){
+//         console.log(this)
+//     }
+// }
