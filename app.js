af86511cdcfd0f8f018cdb648dd3633bc1121982
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-08 20:19:29 +0530

    arrow functions

--- app.js
+++ app.js
@@ -1,46 +1,17 @@
-// class Car{
-//     constructor(model,make){
-//         this.make = make;
-//         this.model = model;
-//     }
-
-//     move(){
-//         console.log("car is moving")
-//     }
-// }
-
-// const car1 = new Car('BMW','X5');
-
-const arr = [1,2,3,4,5,6,7,8,9,10];
-
-// const sum = arr.reduce((a,b)=>a+b)
-// const sum = arr.reduce(function(a,b){
-//     // console.log(a,b)
-//     return a*b
-// })
-
-// console.log(sum)
-
-// const newArr = arr.filter((item)=> item % 2 === 0)
-// console.log(newArr)
-
-// const ele = arr.find(function(item){
-//     return item==10
-// })
-
-// const newArr = []
-
-// arr.forEach(function(e){
-//     if(e%2 === 0){
-//         newArr.push(e);
-//     }
-// })
-
-const f = function(name){
-    this.name=name
-    return 10
+const add = function(a=0, b=0) {
+    return a + b;
 }
+const addArrow2 = (a=0, b=0)=> {
+    return a+b;
+}
+const addArrow1 = (a=0, b=0)=> a + b;
 
-const f2 = new f("mercedes")
+const arr = [1,2,3,4,5];
+const arr2 = arr.map(item => item*2);
+const sum = arr.reduce((a,b)=>a+b)
 
-console.log(f2)
+console.log(arr2)
+console.log(sum)
+
+
+// console.log(addArrow(1,2))
