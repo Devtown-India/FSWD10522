9071aca2c64defedd14760b4037c6c9c341dace4
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-11 21:22:17 +0530

    web apis

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-// // const arr1 = [1, 2, 3 ];
-// // const arr = [...arr1]
+setTimeout(()=>{
+    console.log(1)
+}, 0)
 
-// // console.log(arr)
-
-// // console.log(arr1)
-// // console.log(arr2)
-// // console.log(...arr1)
-// // console.log(...arr2,)
-// // const arr3 = [...arr1, ...arr2]
-// // console.log(arr3)
-// // console.log(...arr3)
-
-// const car = {
-//     brand:"mercedes"
-// }
-// const engine ={
-//     type:"v8"
-// }
-// console.log({...car, ...engine})
-
-const Name = "John"
-const age = 21
-console.log(Name +" is "+ age+" years old")
-console.log(`${Name} is ${age>20 ? "OOOOOLLLDD":"YOUNNG"} years old`)
+for(let i =0;i<10000;i++){
+    console.log("loop")
+}
+for(let i =0;i<10000;i++){
+    console.log("loop")
+}
+for(let i =0;i<10000;i++){
+    console.log("loop")
+}
+for(let i =0;i<10000;i++){
+    console.log("loop")
+}
+console.log(2)
