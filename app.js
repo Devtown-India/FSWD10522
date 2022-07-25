7aa404130955c5c3da62a2dd590434f36015cf5c
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-25 20:05:04 +0530

    async and await

--- app.js
+++ app.js
@@ -1,48 +1,22 @@
-// ele.innerHTML = "Hello World"
-// ele.innerHTML = "Hello people"
-// const ele = document.getElementsByClassName("list")
-// const ele = document.querySelectorAll("ul")
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then((todos) => {
+//     todos.map((todo) => {
+//       console.log(todo.title);
+//     });
+//   });
 
-// console.log(ele)
-
-// const todos = ['Eat Food','Go for a run',"Workout",'sleep']
-// todos.map(todo=>{
-//     const toAdd = document.createElement('li')
-//     // const text = document.createTextNode(todo)
-//     // toAdd.appendChild(text)
-//     toAdd.innerHTML = todo
-//     list.appendChild(toAdd)
-// })
-
-// ele.innerHTML="modified by javscript"
-// ele.style=`
-// color:dodgerblue; 
-// font-size:30px;
-// font-family:Metropolis
-// `
-// ele.classList.add("new-class")
-// ele.classList.add("nsomething")
-// ele.classList.remove("new-class")
-// console.log(ele.classList)
-
-const timer = document.querySelector('#timer')
-const secondHand = document.querySelector('div.second')
-let time =0;
-const t = setInterval(()=>{ 
-    time++;
-    secondHand.style=`transform:rotate(${time*6}deg)`
-    timer.innerHTML=time;
-},1000)
-
-const list = document.querySelector('ul')
-
-fetch('https://jsonplaceholder.typicode.com/todos').then(response=>response.json()).then(todos=>{
-    todos.map(todo=>{
-    const toAdd = document.createElement('li')
-    // const text = document.createTextNode(todo)
-    // toAdd.appendChild(text)
-    toAdd.innerHTML = todo.title
-    list.appendChild(toAdd)
-})
-})
-
+const doSomething = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+  // for(let i =0;i<1000;i++){
+  //     console.log('1');
+  // }
+};
+doSomething();
+console.log(3);
