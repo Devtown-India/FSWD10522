38f4970a1dbbf53967ee1a5838be49c9306f2a0b
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-27 20:31:11 +0530

    rock paper scissors game

--- app.js
+++ app.js
@@ -1,22 +1,76 @@
-// fetch("https://jsonplaceholder.typicode.com/todos")
-//   .then((response) => response.json())
-//   .then((todos) => {
-//     todos.map((todo) => {
-//       console.log(todo.title);
-//     });
-//   });
+const scoreComp = document.querySelector("#comp");
+const scoreMe = document.querySelector("#me");
+const messageBox = document.querySelector("#message");
 
-const doSomething = async () => {
-  try {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = await res.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-  // for(let i =0;i<1000;i++){
-  //     console.log('1');
-  // }
-};
-doSomething();
-console.log(3);
+let uS = 0, cS = 0;
+
+const options = {
+    0:"rock",
+    1:"paper",
+    2:"scissors"
+}
+
+const computerResponse = ()=>{
+    const random = Math.floor(Math.random()*3);
+    // return options[random];
+    return random;
+}
+
+const tie =()=>{
+    messageBox.innerHTML="It is a tie";
+}
+
+const userWins =()=>{
+    messageBox.innerHTML="You win";
+    scoreMe.innerHTML=++uS;
+}
+const computerWins =()=>{
+    messageBox.innerHTML="You lost";
+    scoreComp.innerHTML=++cS;
+}
+
+const game = (userChoice)=>{
+    const computerChoice = computerResponse();
+    switch(userChoice){
+        case 0:
+            switch(computerChoice){
+                case 0:
+                    tie();
+                    break;
+                case 1:
+                    computerWins();
+                    break;
+                case 2:
+                    userWins();
+                    break;
+            }
+            break;
+        case 1:
+            switch(computerChoice){
+                case 0:
+                    userWins();
+                    break;
+                case 1:
+                    tie();
+                    break;
+                case 2:
+                    computerWins();
+                    break;
+            }
+            break;
+        case 2:
+            switch(computerChoice){
+                case 0:
+                    computerWins();
+                    break;
+                case 1:
+                    userWins();
+                    break;
+                case 2:
+                    tie();
+                    break;
+            }
+            break;
+    }
+    
+}
